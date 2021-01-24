4c11d8906f625c360e28f388f78c4bcaae0ec7bf
Keith <keithwhisper@gmail.com>
2021-01-25 01:15:39 +0800

    chore: enable husky

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,11 +9,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-      </main>
+      <main className={styles.main}></main>
 
-      <footer className={styles.footer}>
-      </footer>
+      <footer className={styles.footer}></footer>
     </div>
   )
 }
