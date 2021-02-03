4d10eccb679679af309d74f42d6c00b641ace3cc
Keith <keithwhisper@gmail.com>
2021-02-03 18:07:25 +0800

    feat: remove custom style

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -4,7 +4,7 @@ import Search from 'components/Search'
 const Header = () => (
   <header className="flex bg-primary p-4 sticky top-0 left-0">
     <Link href="/">
-      <a className="text-lg flex-1 text-white">Agera</a>
+      <a className="text-lg flex-1 text-white font-bold no-underline">Agera</a>
     </Link>
     <div className="flex flex-1">
       <Search />
