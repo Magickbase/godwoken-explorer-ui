a565a54079b03d2818918ce6f0f8b0ca55335673
Keith <keithwhisper@gmail.com>
2021-03-09 01:11:54 +0800

    feat: add some meta info in head

--- components/Head.tsx
+++ components/Head.tsx
@@ -3,8 +3,12 @@ import { EXPLORER_TITLE } from 'utils'
 
 const Head = () => (
   <NextHead>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>{EXPLORER_TITLE}</title>
+
     <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+    <meta name="description" content={EXPLORER_TITLE} />
   </NextHead>
 )
 
