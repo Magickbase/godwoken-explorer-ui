109087b9f3b6dd35d26075ba7ea9c884fe6cbdb9
Keith <keithwhisper@gmail.com>
2021-12-10 14:43:17 +0800

    feat: show icon of udt

--- components/AssetList.tsx
+++ components/AssetList.tsx
@@ -1,7 +1,7 @@
 import type { TFunction } from 'next-i18next'
-import { formatBalance } from 'utils'
+import { API, formatBalance } from 'utils'
 
-const AssetList = ({ assetList = [], t }: { assetList: Array<Record<'name' | 'balance', string>>; t: TFunction }) => (
+const AssetList = ({ assetList = [], t }: { assetList: Array<API.Account.UDT>; t: TFunction }) => (
   <div className="card-container">
     <h2 className="card-subheader normal-case" aria-label={t('userDefinedAssets')}>
       {`${t('userDefinedAssets')}:`}
@@ -11,8 +11,15 @@ const AssetList = ({ assetList = [], t }: { assetList: Array<Record<'name' | 'ba
       {assetList.length ? (
         assetList.map((asset, i) => (
           <div key={asset.name} className="card-field" attr-last={`${i === assetList.length - 1}`}>
+            {asset.icon ? (
+              <img src={asset.icon} className="w-10 h-10 rounded-full mr-2" />
+            ) : (
+              <div className="flex justify-center items-center w-10 h-10 rounded-full mr-2 bg-gray-100 text-lg">
+                {asset.name[0]}
+              </div>
+            )}
             <span className="card-label uppercase">{asset.name}</span>
-            {formatBalance(asset.balance)}
+            <div className="flex-1 text-right">{formatBalance(asset.balance)}</div>
           </div>
         ))
       ) : (
