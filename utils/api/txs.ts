a6185cfe2bc79b7aa91d6d6c130f6cf7ef022ab7
Keith <keithwhisper@gmail.com>
2022-01-20 22:49:35 +0800

    feat: add default value of query.page

--- utils/api/txs.ts
+++ utils/api/txs.ts
@@ -162,6 +162,6 @@ export const getERC20TransferListRes = (list: {
 export const fetchERC20TransferList = (
   query: Partial<Record<'page' | 'udt_address' | 'eth_address', string>>,
 ): Promise<{ txs: Array<Erc20Parsed>; page: string; totalCount: string }> =>
-  fetch(`${SERVER_URL}/transfers?${new URLSearchParams(query)}`)
+  fetch(`${SERVER_URL}/transfers?${new URLSearchParams({ ...query, page: query.page || '1' })}`)
     .then(res => pretreat<{ txs: Array<Erc20Raw>; page: string; total_count: string }>(res))
     .then(getERC20TransferListRes)
