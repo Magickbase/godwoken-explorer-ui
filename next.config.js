cca70f998a4379e633d9cf1d427a798b95c0a318
Keith <keithwhisper@gmail.com>
2022-01-25 14:36:41 +0800

    feat: add /address rewriting to /account

--- next.config.js
+++ next.config.js
@@ -3,4 +3,12 @@ const { i18n } = require('./next-i18next.config')
 module.exports = {
   i18n,
   experimental: { esmExternals: true },
+  async rewrites() {
+    return [
+      {
+        source: '/address/:addr*',
+        destination: '/account/:addr*',
+      },
+    ]
+  },
 }
