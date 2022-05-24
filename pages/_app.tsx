d301596adf97bdcb404a1a688b4438a24c7c706a
qiweiii <yangqiwei97@gmail.com>
2022-05-24 12:21:10 +0800

    refactor: self host fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,11 @@ import { AppProps } from 'next/app'
 import ErrorBoundary from 'components/ErrorBoundary'
 import { theme } from '../utils'
 import '../styles/globals.css'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+import '@fontsource/material-icons'
 
 const Agera = ({ Component, pageProps }: AppProps) => {
   const [loading, setLoading] = useState(0)
