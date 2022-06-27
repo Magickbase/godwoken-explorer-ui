2491d3d94a8e7a201a56e19c8be7d014a3ece458
Keith <keithwhisper@gmail.com>
2022-06-27 15:38:19 +0800

    feat: display unknown account
    This commit enables displaying a blank unknown account instead of
    redirecting to a 404 not-found page when target account is not generated
    on godwoken.

--- components/AccountOverview.tsx
+++ components/AccountOverview.tsx
@@ -8,6 +8,7 @@ import MetaContract from 'components/MetaContract'
 import SmartContract from 'components/SmartContract'
 import Polyjuice from 'components/Polyjuice'
 import SUDT from 'components/SUDT'
+import UnknownAccount from 'components/UnknownAccount'
 import { GCKB_DECIMAL, GraphQLSchema, client } from 'utils'
 
 export type BasicScript = Record<'args' | 'code_hash' | 'hash_type', string>
@@ -16,6 +17,10 @@ interface AccountBase {
   script_hash: string
   transaction_count: number
   nonce: number
+}
+
+interface UnknownUser extends AccountBase {
+  type: GraphQLSchema.AccountType.Uknown
 }
 
 interface EthUser extends AccountBase {
@@ -70,7 +75,7 @@ export interface MetaContract extends AccountBase {
 }
 
 export type AccountOverviewProps = {
-  account: EthUser | EthAddrReg | PolyjuiceCreator | PolyjuiceContract | Udt | MetaContract
+  account: EthUser | EthAddrReg | PolyjuiceCreator | PolyjuiceContract | Udt | MetaContract | UnknownUser
   balance: string
   deployerAddr?: string
 }
@@ -123,7 +128,17 @@ const deployAddrQuery = gql`
 type Variables = { address: string } | { script_hash: string }
 
 export const fetchAccountOverview = (variables: Variables) =>
-  client.request<Omit<AccountOverviewProps, 'balance'>>(accountOverviewQuery, variables).then(data => data.account)
+  client.request<Omit<AccountOverviewProps, 'balance'>>(accountOverviewQuery, variables).then(
+    data =>
+      data.account ??
+      ({
+        type: GraphQLSchema.AccountType.Uknown,
+        eth_address: variables['eth_address'] ?? null,
+        script_hash: variables['script_hash'] ?? '',
+        transaction_count: 0,
+        nonce: 0,
+      } as UnknownUser),
+  )
 
 export const fetchAccountBalance = (variables: { address_hashes: Array<string> } | { script_hashes: Array<string> }) =>
   client
@@ -189,6 +204,7 @@ const AccountOverview: React.FC<AccountOverviewProps> = ({ account, balance, dep
           {account.type === GraphQLSchema.AccountType.Udt && account.udt ? (
             <SUDT udt={account.udt} script={account.script} script_hash={account.script_hash} />
           ) : null}
+          {account.type === GraphQLSchema.AccountType.Uknown ? <UnknownAccount nonce={account.nonce} /> : null}
         </Grid>
       </Grid>
     </Paper>
---
+++ components/UnknownAccount.tsx
@@ -0,0 +1,34 @@
+import { useTranslation } from 'next-i18next'
+import { List, ListItem, ListItemText, ListSubheader, Divider, Typography } from '@mui/material'
+
+const User = ({ nonce }: { nonce: number }) => {
+  const [t] = useTranslation('account')
+
+  const fields = [
+    {
+      label: t(`type`),
+      value: <Typography variant="body2">Uknown</Typography>,
+    },
+    { label: t('nonce'), value: <Typography variant="body2">{nonce.toLocaleString('en')}</Typography> },
+  ]
+
+  return (
+    <List
+      subheader={
+        <ListSubheader component="div" sx={{ textTransform: 'capitalize', bgcolor: 'transparent' }}>
+          {t('basicInfo')}
+        </ListSubheader>
+      }
+      sx={{ textTransform: 'capitalize' }}
+    >
+      <Divider variant="middle" />
+      {fields.map(field => (
+        <ListItem key={field.label}>
+          <ListItemText primary={field.label} secondary={field.value} />
+        </ListItem>
+      ))}
+    </List>
+  )
+}
+
+export default User
