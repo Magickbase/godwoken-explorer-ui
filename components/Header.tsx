a6e2f115b3ada1c2b1be97fd9b33f2cfbad957c2
Keith <keithwhisper@gmail.com>
2022-07-04 20:34:45 +0800

    feat: add v0, v1 links

--- components/Header.tsx
+++ components/Header.tsx
@@ -76,8 +76,13 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 }))
 
 const TOKEN_TYPE_LIST = ['bridge', 'native']
-const CHAIN_TYPE_LIST = GW_VERSION ? ['testnet'] : ['mainnet', 'testnet']
 const LOCALE_LIST = ['zh-CN', 'en-US']
+const CHAIN_LINKS = [
+  { label: 'mainnet_v1', href: 'https://v1.gwscan.com' },
+  { label: 'testnet_v1', href: 'https://v1.testnet.gwscan.com' },
+  { label: 'mainnet_v0', href: 'https://gwscan.com' },
+  { label: 'testnet_v0', href: 'https://pudge.gwscan.com' },
+]
 
 const Header = () => {
   const [t, { language }] = useTranslation('common')
@@ -188,17 +193,23 @@ const Header = () => {
       >
         {t(`chainType`)}
       </Typography>
-      {CHAIN_TYPE_LIST.map(chain => {
-        const url = `https://${
-          chain === 'mainnet'
-            ? process.env.NEXT_PUBLIC_MAINNET_EXPLORER_HOSTNAME
-            : process.env.NEXT_PUBLIC_TESTNET_EXPLORER_HOSTNAME
-        }/${language}`
+      {CHAIN_LINKS.map(chain => {
+        const url = `${chain.href}/${language}`
+        const [label, version] = chain.label.split('_')
         return (
-          <MenuItem key={chain} onClick={handleMenuListClose} sx={{ p: 0 }}>
+          <MenuItem key={chain.label} onClick={handleMenuListClose} sx={{ p: 0 }}>
             <NextLink href={url}>
-              <Link href={url} title={t(chain)} underline="none" sx={{ width: '100%', padding: '6px 16px' }}>
-                {t(chain)}
+              <Link
+                href={url}
+                title={`${t(label)} ${version}`}
+                underline="none"
+                display="flex"
+                justifyContent="space-between"
+                minWidth="105px"
+                sx={{ width: '100%', padding: '6px 16px' }}
+              >
+                <span>{t(label)}</span>
+                <span>{version}</span>
               </Link>
             </NextLink>
           </MenuItem>
