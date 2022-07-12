c218439e2d80c05b36d7cbce7c336871757ec4d4
Keith <keithwhisper@gmail.com>
2022-07-12 20:51:58 +0800

    feat: add writing to contract

--- components/Head.tsx
+++ components/Head.tsx
@@ -6,7 +6,7 @@ const DESCRIPTION = 'A blockchain explorer for godwoken'
 const Head = () => (
   <NextHead>
     <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <meta name="description" content={DESCRIPTION} />
 
     <meta property="og:title" content={EXPLORER_TITLE} />
