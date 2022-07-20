f9d23d669a8fe8fee49e53538f118eabeaef92b4
qiweiii <yangqiwei97@gmail.com>
2022-07-20 23:01:55 +0800

    feat: charts wip

---
+++ components/ChartComponents/index.tsx
@@ -0,0 +1,44 @@
+import styles from './styles.module.scss'
+
+export const CustomTooltip: React.FC<any> = props => {
+  const { active, payload, label, isMobile, theme } = props
+  if (active && payload && payload.length) {
+    return (
+      <div
+        className={styles.content}
+        style={{
+          border: 'none',
+          borderRadius: isMobile ? 4 : 8,
+          boxShadow: isMobile ? '0px 3px 10px rgba(0, 0, 0, 0.1)' : '0px 6px 20px rgba(0, 0, 0, 0.1)',
+          padding: isMobile ? '12px 8px 10px' : '16px 16px 14px',
+          backgroundColor: '#fff',
+        }}
+      >
+        <div style={{ color: '#666' }}>{`${label}`}</div>
+        {payload.map(({ name, value }) => (
+          <li key={name} style={{ color: theme.palette.secondary.main, paddingLeft: 12 }}>{`${name}: ${value}`}</li>
+        ))}
+      </div>
+    )
+  }
+
+  return null
+}
+
+export const CustomYLabel: React.FC<any> = ({ value, theme, isMobile, align = 'left' }) => {
+  const y = align === 'left' ? (isMobile ? '6%' : '10%') : isMobile ? '90%' : '254%'
+
+  return (
+    <text
+      x={isMobile ? '-165' : '-205'}
+      y={y}
+      fill={theme.palette.secondary.light}
+      color={theme.palette.secondary.light}
+      fontSize={isMobile ? 12 : 14}
+      textAnchor="middle"
+      style={{ transform: 'rotate(-90deg)' }}
+    >
+      {value}
+    </text>
+  )
+}
--- pages/charts.tsx
+++ pages/charts.tsx
@@ -3,14 +3,28 @@ import { GetServerSideProps } from 'next'
 import dayjs from 'dayjs'
 import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, Brush } from 'recharts'
-import { Container, Stack, Paper, Box, colors, Typography, Alert } from '@mui/material'
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+  Brush,
+  LineChart,
+  Line,
+} from 'recharts'
+import { Container, Stack, Paper, Box, Typography, Alert } from '@mui/material'
 import SubpageHead from 'components/SubpageHead'
-// import PageTitle from 'components/PageTitle'
-import { fetchDailyData, getDailyDataRes, handleApiError, theme, GAS_UNIT } from 'utils'
+import { CustomYLabel as YLabel, CustomTooltip } from 'components/ChartComponents'
+import ClearIcon from '../assets/icons/clear.svg'
+import { fetchDailyData, getDailyDataRes, handleApiError, GAS_UNIT, theme } from 'utils'
+import { useTheme } from '@mui/material/styles'
+import useMediaQuery from '@mui/material/useMediaQuery'
 
 const bgColors = ['#fff', '#fff']
-const accentColors = [colors.lightBlue['A700'], colors.cyan['A700']]
+const accentColors = [theme.palette.primary.main, '#28B959']
 
 type State = { dailyData: ReturnType<typeof getDailyDataRes> }
 const keysList = [
@@ -26,8 +40,11 @@ const keysList = [
 ]
 
 const Charts = (initState: State) => {
-  const [charts, setCharts] = useState(initState)
   const [t] = useTranslation('charts')
+  const theme = useTheme()
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
+  const [charts, setCharts] = useState(initState)
+  const [alertOpen, setAlertOpen] = useState(true)
 
   useEffect(() => {
     setCharts(initState)
@@ -37,91 +54,149 @@ const Charts = (initState: State) => {
   return (
     <>
       <SubpageHead subtitle={title} />
-      <Container sx={{ py: 6 }}>
-        <Alert severity="info" sx={{ marginBottom: 4 }}>
+      <Container sx={{ pt: 2, pb: { xs: 5.5, md: 11 }, px: { xs: 2, sm: 3, md: 2, lg: 0 } }}>
+        <Alert
+          severity="info"
+          sx={{
+            'display': alertOpen ? 'flex' : 'none',
+            'borderRadius': { xs: 2, md: 4 },
+            'fontSize': { xs: 12, md: 14 },
+            'mb': { xs: 2.5, md: 3.75 },
+            'px': { xs: 1.5, md: 2 },
+            'height': { xs: 36, md: 40 },
+            'bgcolor': '#FFF5E5',
+            'color': '#FF9900',
+            'alignItems': 'center',
+            '& .MuiAlert-icon': {
+              color: '#FF9900',
+              mr: { xs: 1.5, md: 2 },
+            },
+            '& .MuiSvgIcon-root': {
+              width: { xs: 14, md: 20 },
+              height: { xs: 14, md: 20 },
+            },
+            '& .MuiAlert-action ': {
+              alignItems: 'center',
+              cursor: 'pointer',
+              p: 0,
+              mr: 0,
+              svg: {
+                width: { xs: 14, md: 19 },
+                height: { xs: 14, md: 19 },
+              },
+            },
+          }}
+          action={<ClearIcon onClick={() => setAlertOpen(false)} />}
+        >
           {t(`updateTimeInfo`)}
         </Alert>
-        {/* <PageTitle>{title}</PageTitle> */}
-        <Stack spacing={2}>
+        <Stack spacing={{ xs: 3, md: 5 }}>
           {keysList.map(keys => (
-            <Paper key={keys.map(k => k.key).join()} elevation={0}>
-              <Box sx={{ height: '300px', bgcolor: bgColors[0] }} p={4}>
-                <Typography variant="h5" align="center">
-                  {t(`title.${keys.map(k => k.key).join('/')}`)}
-                </Typography>
+            <Box key={keys.map(k => k.key).join()}>
+              <Typography
+                align="left"
+                fontWeight={500}
+                color="secondary"
+                fontSize={{ xs: 15, md: 20 }}
+                mb={{ xs: 1.8, md: 3 }}
+              >
+                {t(`title.${keys.map(k => k.key).join('/')}`)}
+              </Typography>
+              <Paper
+                elevation={0}
+                sx={{
+                  height: { xs: 376, md: 456 },
+                  bgcolor: bgColors[0],
+                  borderRadius: { xs: 2, md: 4 },
+                  border: 'solid #f0f0f0',
+                  borderWidth: { xs: 0.5, md: 1 },
+                }}
+              >
                 <ResponsiveContainer width="100%" height="100%">
-                  <AreaChart
+                  <LineChart
                     width={500}
                     height={300}
                     data={charts.dailyData}
-                    margin={{ top: 50, right: 30, left: 30, bottom: 0 }}
+                    margin={
+                      isMobile
+                        ? { top: 32, right: 12, left: 8, bottom: 20 }
+                        : { top: 48, right: 108, left: 100, bottom: 48 }
+                    }
                   >
-                    <defs>
-                      {keys.map(({ key }, idx) => (
-                        <linearGradient key={key} id={`color-${key}`} x1="0" y1="0" x2="0" y2="1">
-                          <stop offset="5%" stopColor={accentColors[idx]} stopOpacity={0.3} />
-                          <stop offset="95%" stopColor={accentColors[idx]} stopOpacity={0.1} />
-                        </linearGradient>
-                      ))}
-                    </defs>
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="date" tick={{ fontSize: 12 }} />
+                    <CartesianGrid strokeDasharray="4" />
+                    <XAxis
+                      dataKey="date"
+                      tick={{ fontSize: isMobile ? 11 : 12, color: theme.palette.secondary.main }}
+                      tickLine={false}
+                      minTickGap={30}
+                      axisLine={false}
+                      interval="preserveStartEnd"
+                    />
                     {keys.map(({ key, unit }, idx) => (
                       <YAxis
                         label={{
-                          value: t(key),
-                          position: idx ? 'insideTopRight' : 'insideTopLeft',
-                          offset: '-30',
+                          content:
+                            idx === 0 ? (
+                              <YLabel theme={theme} isMobile={isMobile} value={t(key)} />
+                            ) : (
+                              <YLabel theme={theme} isMobile={isMobile} value={t(key)} align="right" />
+                            ),
                         }}
                         unit={unit}
-                        key={key}
+                        key={key + '-yaxis'}
                         yAxisId={idx}
                         orientation={idx ? 'right' : 'left'}
-                        tick={{ fontSize: 12 }}
+                        tick={{ fontSize: isMobile ? 11 : 12, color: theme.palette.secondary.main }}
+                        tickLine={false}
+                        axisLine={false}
+                        interval="preserveStartEnd"
                       />
                     ))}
-                    {keys.map(({ key, unit }) => (
-                      <Tooltip key={key} formatter={value => (unit ? `${value} ${unit}` : value)} />
+                    {keys.map(({ key }) => (
+                      <Tooltip
+                        key={key}
+                        wrapperStyle={{ fontSize: isMobile ? 13 : 14 }}
+                        content={<CustomTooltip theme={theme} isMobile={isMobile} />}
+                        offset={12}
+                      />
                     ))}
-                    <Legend />
                     <Brush height={20} tickFormatter={() => ''} stroke={theme.palette.primary.light}>
                       <AreaChart height={30} data={charts.dailyData}>
-                        <defs>
-                          {keys.map(({ key }, idx) => (
-                            <linearGradient key={key} id={`color-${key}`} x1="0" y1="0" x2="0" y2="1">
-                              <stop offset="5%" stopColor={accentColors[idx]} stopOpacity={0.3} />
-                              <stop offset="95%" stopColor={accentColors[idx]} stopOpacity={0.1} />
-                            </linearGradient>
-                          ))}
-                        </defs>
                         {keys.map(({ key }, idx) => (
                           <Area
                             key={key}
                             type="monotone"
                             dataKey={key}
                             stroke={accentColors[idx]}
-                            fill={`url(#color-${key})`}
-                            fillOpacity={1}
+                            fill="#ccc"
+                            fillOpacity={0.2}
                           />
                         ))}
                       </AreaChart>
                     </Brush>
                     {keys.map(({ key }, idx) => (
-                      <Area
+                      <Line
                         yAxisId={idx}
+                        dot={false}
                         name={t(key)}
                         key={key}
                         type="monotone"
                         dataKey={key}
+                        legendType="circle"
                         stroke={accentColors[idx]}
-                        fill={`url(#color-${key})`}
-                        fillOpacity={1}
+                        strokeWidth={2}
+                        activeDot={{
+                          stroke: '#fff',
+                          strokeWidth: isMobile ? 4.5 : 8,
+                          r: isMobile ? 6 : 10,
+                          opacity: 0.9,
+                        }}
                       />
                     ))}
-                  </AreaChart>
+                  </LineChart>
                 </ResponsiveContainer>
-              </Box>
-            </Paper>
+              </Paper>
+            </Box>
           ))}
         </Stack>
       </Container>
@@ -132,7 +207,6 @@ const Charts = (initState: State) => {
 export const getServerSideProps: GetServerSideProps<State> = async ({ locale, res }) => {
   try {
     const today = dayjs().format('YYYY-MM-DD')
-    // const startDay = '2021-11-31'
     const startDay = '2021-11-30'
 
     const [dailyData, lng] = await Promise.all([
