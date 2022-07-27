be6d9db27164bb3c9018e6d3e22d7f4ef6b70259
Qiwei Yang <yangqiwei97@gmail.com>
2022-07-27 15:50:21 +0800

    feat: update-ui, charts page (#340)

---
+++ components/ChartComponents/index.tsx
@@ -0,0 +1,36 @@
+import styles from './styles.module.scss'
+
+export const CustomTooltip: React.FC<any> = ({ active, payload, label, theme }) => {
+  if (active && payload && payload.length) {
+    return (
+      <div className={styles.tooltipContent}>
+        <div>{`${label}`}</div>
+        <ul>
+          {payload.map(({ name, value }, idx) => (
+            <li key={name} style={{ color: idx === 0 ? theme.palette.primary.main : '#28b959' }}>
+              <span>{`${name}: ${value}`}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+  return null
+}
+
+export const CustomYLabel: React.FC<any> = ({ value, theme, isMobile, align = 'left' }) => {
+  const y = align === 'left' ? (isMobile ? '6%' : '10%') : isMobile ? '90%' : '254%'
+  return (
+    <text
+      x={isMobile ? '-150' : '-180'}
+      y={y}
+      fill={theme.palette.secondary.light}
+      color={theme.palette.secondary.light}
+      fontSize={isMobile ? 12 : 14}
+      textAnchor="middle"
+      style={{ transform: 'rotate(-90deg)' }}
+    >
+      {value}
+    </text>
+  )
+}
