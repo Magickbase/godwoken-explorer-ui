3984ac41cae614d4ad4369a18e1e9f263d512042
qiweiii <yangqiwei97@gmail.com>
2022-07-29 16:59:10 +0800

    feat: txlist and tokens list

--- pages/txs.tsx
+++ pages/txs.tsx
@@ -3,25 +3,15 @@ import { useRouter } from 'next/router'
 import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useQuery } from 'react-query'
-import {
-  Container,
-  Paper,
-  Typography,
-  Box,
-  Stack,
-  TableContainer,
-  TableRow,
-  Table,
-  TableCell,
-  TableHead,
-  TableBody,
-  Skeleton,
-} from '@mui/material'
+import { Container, Typography, Box, Stack, TableContainer, Skeleton } from '@mui/material'
+import { useTheme } from '@mui/material/styles'
+import useMediaQuery from '@mui/material/useMediaQuery'
+import Table from 'components/Table'
 import PageTitle from 'components/PageTitle'
-import TxListComp, { fetchTxList } from 'components/TxList'
 import SubpageHead from 'components/SubpageHead'
-import Pagination from 'components/SimplePagination'
+import Pagination from 'components/Pagination'
 import { SIZES } from 'components/PageSize'
+import TxListComp, { fetchTxList } from 'components/TxList'
 import { PCKB_SYMBOL } from 'utils'
 
 const TxList = () => {
@@ -29,7 +19,6 @@ const TxList = () => {
   const {
     query: { page_size = SIZES[1], before = null, after = null, block_from = null, block_to = null },
   } = useRouter()
-
   const { isLoading, data: txList } = useQuery(['transactions', before, after, block_from, block_to], () =>
     fetchTxList({
       limit: +page_size as number,
@@ -39,70 +28,91 @@ const TxList = () => {
       end_block_number: block_to ? +block_to : null,
     }),
   )
+  const theme = useTheme()
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
 
   const title = t('tx_list_title')
   if (isLoading || !txList) {
     return (
       <>
         <SubpageHead subtitle={title} />
-        <Container sx={{ px: 1, py: 2 }}>
+        <Container sx={{ px: { xs: 2, sm: 3, md: 2, lg: 0 }, pb: { xs: 5.5, md: 11 } }}>
           <PageTitle>
             <Typography variant="inherit" display="inline" pr={1}>
               {title}
             </Typography>
           </PageTitle>
-          <Paper>
-            <Box sx={{ px: 1, py: 2 }}>
-              <Stack direction="row" justifyContent="space-between" alignItems="center" py={1}>
-                <Skeleton animation="wave" width={50} height={24} />
-                <Skeleton animation="wave" width={50} height={24} />
-              </Stack>
-              <TableContainer>
-                <Table size="small">
-                  <TableHead sx={{ textTransform: 'capitalize' }}>
-                    <TableRow>
-                      <TableCell component="th">{t('txHash')}</TableCell>
-                      <TableCell component="th">
-                        <Stack direction="row" alignItems="center" whiteSpace="nowrap">
-                          {t('block')}
-                        </Stack>
-                      </TableCell>
-                      <TableCell component="th">{t('age')} </TableCell>
-                      <TableCell sx={{ display: { xs: 'none', md: 'table-cell' } }} component="th">
-                        {t('from')}
-                      </TableCell>
-                      <TableCell sx={{ display: { xs: 'none', md: 'table-cell' } }} component="th">
-                        {t('to')}
-                      </TableCell>
-                      <TableCell sx={{ display: { xs: 'table-cell', md: 'none' } }} component="th">
-                        {t('transfer')}
-                      </TableCell>
-                      <TableCell component="th" sx={{ whiteSpace: 'nowrap', textTransform: 'none' }}>{`${t(
-                        'value',
-                      )} (${PCKB_SYMBOL})`}</TableCell>
-                      <TableCell component="th">{t('type')}</TableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {Array.from({ length: +page_size }).map((_, idx) => (
-                      <TableRow key={idx}>
-                        <TableCell colSpan={7} sx={{ display: { xs: 'table-cell', md: 'none' } }}>
-                          <Skeleton animation="wave" height={25} />
-                        </TableCell>
-                        <TableCell colSpan={8} sx={{ display: { xs: 'none', md: 'table-cell' } }}>
-                          <Skeleton animation="wave" height={25} />
-                        </TableCell>
-                      </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-              </TableContainer>
-              <Stack direction="row" justifyContent="space-between" alignItems="center" py={1}>
-                <Skeleton animation="wave" width={50} height={24} />
-                <Skeleton animation="wave" width={50} height={24} />
-              </Stack>
-            </Box>
-          </Paper>
+          <Box
+            sx={{
+              borderRadius: {
+                xs: 2,
+                sm: 4,
+              },
+              border: {
+                xs: '0.5px solid #F0F0F0',
+                md: '1px solid #F0F0F0',
+              },
+              pt: { xs: 1.5, md: 2 },
+              pb: 2,
+              mt: { xs: 2, md: 3 },
+              bgcolor: '#fff',
+            }}
+          >
+            <Stack
+              direction="row"
+              flexWrap="wrap"
+              justifyContent="space-between"
+              alignItems="center"
+              mb={{ xs: 1.5, md: 2 }}
+              px={{ xs: 1.5, md: 3 }}
+            >
+              <Skeleton animation="wave" width={50} height={24} />
+              <Skeleton animation="wave" width={50} height={24} />
+            </Stack>
+            <TableContainer>
+              <Table>
+                <thead style={{ textTransform: 'capitalize', fontSize: isMobile ? 12 : 14 }}>
+                  <tr>
+                    <th>{t('txHash')}</th>
+                    <th>
+                      <Stack direction="row" alignItems="center" whiteSpace="nowrap">
+                        {t('block')}
+                      </Stack>
+                    </th>
+                    <th>{t('age')} </th>
+                    <th style={{ display: isMobile ? 'none' : 'table-cell' }}>{t('from')}</th>
+                    <th style={{ display: isMobile ? 'none' : 'table-cell' }}>{t('to')}</th>
+                    <th style={{ display: isMobile ? 'none' : 'table-cell' }}>{t('transfer')}</th>
+                    <th style={{ whiteSpace: 'nowrap', textTransform: 'none' }}>{`${t('value')} (${PCKB_SYMBOL})`}</th>
+                    <th>{t('type')}</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {Array.from({ length: +page_size }).map((_, idx) => (
+                    <tr key={idx}>
+                      <td colSpan={7} style={{ display: isMobile ? 'table-cell' : 'none' }}>
+                        <Skeleton animation="wave" height={25} />
+                      </td>
+                      <td colSpan={8} style={{ display: isMobile ? 'none' : 'table-cell' }}>
+                        <Skeleton animation="wave" height={25} />
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            </TableContainer>
+            <Stack
+              direction="row"
+              flexWrap="wrap"
+              justifyContent="space-between"
+              alignItems="center"
+              mt={{ xs: 1.5, md: 2 }}
+              px={{ xs: 1.5, md: 3 }}
+            >
+              <Skeleton animation="wave" width={50} height={24} />
+              <Skeleton animation="wave" width={50} height={24} />
+            </Stack>
+          </Box>
         </Container>
       </>
     )
@@ -111,28 +121,50 @@ const TxList = () => {
   return (
     <>
       <SubpageHead subtitle={title} />
-      <Container sx={{ px: 1, py: 2 }}>
+      <Container sx={{ px: { xs: 2, sm: 3, md: 2, lg: 0 }, pb: { xs: 5.5, md: 11 } }}>
         <PageTitle>
           <Typography variant="inherit" display="inline" pr={1}>
             {title}
           </Typography>
         </PageTitle>
-        <Paper>
-          <Box sx={{ px: 1, py: 2 }}>
-            {txList.entries.length >= 2 ? (
-              <Stack direction="row" justifyContent="sapce-between" alignItems="center">
-                <Typography variant="inherit" flex="1">
-                  {t(`tx_in_block_from_to`, {
-                    to: txList.entries[0].block.number,
-                    from: txList.entries[txList.entries.length - 1].block.number,
-                  })}
-                </Typography>
-                <Pagination {...txList.metadata} />
-              </Stack>
-            ) : null}
-            <TxListComp transactions={txList} maxCount="500k" pageSize={+page_size} />
-          </Box>
-        </Paper>
+        <Box
+          sx={{
+            borderRadius: {
+              xs: 2,
+              sm: 4,
+            },
+            border: {
+              xs: '0.5px solid #F0F0F0',
+              md: '1px solid #F0F0F0',
+            },
+            pt: { xs: 1.5, md: 2 },
+            pb: 2,
+            mt: { xs: 2, md: 3 },
+            bgcolor: '#fff',
+          }}
+        >
+          {txList.entries.length >= 2 ? (
+            <Stack
+              direction="row"
+              flexWrap="wrap"
+              justifyContent="space-between"
+              alignItems="center"
+              mb={{ xs: 1.5, md: 2 }}
+              px={{ xs: 1.5, md: 3 }}
+            >
+              <Typography variant="inherit" color="secondary" fontWeight={500} fontSize={{ xs: 14, md: 16 }}>
+                {t(`tx_in_block_from_to`, {
+                  to: txList.entries[0].block.number,
+                  from: txList.entries[txList.entries.length - 1].block.number,
+                })}
+              </Typography>
+              {/* TODO: need update from TxListComp first  */}
+              <Pagination page={1} total={txList.metadata.total_count * +page_size} />
+            </Stack>
+          ) : null}
+
+          <TxListComp transactions={txList} maxCount={isMobile ? '100k' : '500k'} pageSize={+page_size} />
+        </Box>
       </Container>
     </>
   )
