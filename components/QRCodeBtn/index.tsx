c5845b04b2373495238e41978c646ce0596c5ae5
qiweiii <yangqiwei97@gmail.com>
2022-08-06 18:45:37 +0800

    feat: add save button

--- components/QRCodeBtn/index.tsx
+++ components/QRCodeBtn/index.tsx
@@ -1,17 +1,21 @@
 /* eslint-disable @next/next/no-img-element */
 import { useState } from 'react'
 import { useTranslation } from 'next-i18next'
-import { Box, Modal, Typography } from '@mui/material'
+import { Button, Modal } from '@mui/material'
 import CopyIcon from 'assets/icons/qr-code.svg'
 import CloseIcon from 'assets/icons/close.svg'
 import styles from './styles.module.scss'
 import Tooltip from 'components/Tooltip'
 import QRCode from 'qrcode'
+import { useTheme } from '@mui/material/styles'
+import useMediaQuery from '@mui/material/useMediaQuery'
 
 const QRCodeBtn: React.FC<{ content: string }> = ({ content }) => {
   const [t] = useTranslation('common')
   const [open, setOpen] = useState(false)
   const [data, setData] = useState('')
+  const theme = useTheme()
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
 
   const handleOpen = async () => {
     const dataUrl = await generateQR(content)
@@ -27,6 +31,15 @@ const QRCodeBtn: React.FC<{ content: string }> = ({ content }) => {
     }
   }
 
+  const handleSave = () => {
+    const link = document.createElement('a')
+    link.download = content
+    link.href = data
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+  }
+
   return (
     <>
       <Tooltip title={t(`clickViewQRCode`)} placement="bottom">
@@ -37,10 +50,27 @@ const QRCodeBtn: React.FC<{ content: string }> = ({ content }) => {
       <Modal open={open} onClose={() => setOpen(false)}>
         <div className={styles.qrCodeModal}>
           <div className={styles.close} onClick={() => setOpen(false)}>
-            <CloseIcon fontSize="inherit" />
+            <CloseIcon fontSize={14} />
           </div>
           <img src={data} alt="qr-code-image" width={128} height={128} />
-          <p id="modal-modal-title">{content}</p>
+          <p className="mono-font">{content}</p>
+          {isMobile && (
+            <Button
+              variant="contained"
+              sx={{
+                mt: 3,
+                borderRadius: 6,
+                textTransform: 'none',
+                fontSize: 12,
+                width: 160,
+                height: 40,
+                boxShadow: 'none',
+              }}
+              onClick={handleSave}
+            >
+              {t('saveQrCode')}
+            </Button>
+          )}
         </div>
       </Modal>
     </>
