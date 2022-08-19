d407f15cfb27b335eab5d9bfcf5dc490ad5e6f08
Chen Yu <keithwhisper@gmail.com>
2022-08-19 23:55:33 +0800

    fix: fix style of empty list (#417)

--- components/ContractEventsList.tsx
+++ components/ContractEventsList.tsx
@@ -17,8 +17,9 @@ import {
   InputAdornment,
   TextField,
 } from '@mui/material'
-import { ParsedEventLog, IMG_URL, useDebounce } from 'utils'
 import ContractEventListItem from './ContractEventListItem'
+import NoDataIcon from 'assets/icons/no-data.svg'
+import { ParsedEventLog, IMG_URL, useDebounce } from 'utils'
 
 export const EventFilterIcon = ({ setSearchText, tooltip, value }) => (
   <Tooltip title={tooltip} placement="top">
@@ -63,7 +64,7 @@ const ContractEventsList = ({ list }: { list: ParsedEventLog[] }) => {
     )
   }, [debouncedSetListItems, list, searchText])
 
-  return (
+  return listItems?.length ? (
     <Box sx={{ px: 1, py: 2 }}>
       <Stack direction="row" justifyContent="space-between" alignItems="center">
         <Typography fontSize={14} color="#333333" sx={{ m: 1.5 }}>
@@ -113,58 +114,67 @@ const ContractEventsList = ({ list }: { list: ParsedEventLog[] }) => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {listItems?.length ? (
-              listItems?.map((item, idx) => (
-                <TableRow key={item.id + '-' + idx}>
-                  <TableCell>
-                    <Stack>
-                      <NextLink href={`/tx/${item.txHash}`} passHref>
+            {listItems?.map((item, idx) => (
+              <TableRow key={item.id + '-' + idx}>
+                <TableCell>
+                  <Stack>
+                    <NextLink href={`/tx/${item.txHash}`} passHref>
+                      <Link
+                        href={`/tx/${item.txHash}`}
+                        underline="none"
+                        color="secondary"
+                        className="mono-font"
+                        sx={{ fontSize: 14 }}
+                      >
+                        {item.txHash.slice(0, 17)}...
+                      </Link>
+                    </NextLink>
+                    <Box sx={{ display: 'flex' }}>
+                      <NextLink href={`/block/${item.blockNumber}`} passHref>
                         <Link
-                          href={`/tx/${item.txHash}`}
+                          href={`/block/${item.blockNumber}`}
                           underline="none"
                           color="primary"
                           className="mono-font"
                           sx={{ fontSize: 14 }}
                         >
-                          {item.txHash.slice(0, 17)}...
+                          # {item.blockNumber}
                         </Link>
                       </NextLink>
-                      <Box sx={{ display: 'flex' }}>
-                        <NextLink href={`/block/${item.blockNumber}`} passHref>
-                          <Link
-                            href={`/block/${item.blockNumber}`}
-                            underline="none"
-                            color="primary"
-                            sx={{ fontSize: 14 }}
-                          >
-                            # {item.blockNumber}
-                          </Link>
-                        </NextLink>
-                        <Box sx={{ pl: 0.5 }} />
-                        <EventFilterIcon
-                          setSearchText={setSearchText}
-                          tooltip={t('filterEventBy', { filter: `BlockNo=${item.blockNumber}` })}
-                          value={`${item.blockNumber}`}
-                        />
-                      </Box>
-                    </Stack>
-                  </TableCell>
-                  {/* <TableCell>{item.method}</TableCell> */}
-                  <TableCell sx={{ pr: 1 }}>
-                    <ContractEventListItem item={item} setSearchText={setSearchText} />
-                  </TableCell>
-                </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell colSpan={7} align="center">
-                  {t('no_records')}
+                      <Box sx={{ pl: 0.5 }} />
+                      <EventFilterIcon
+                        setSearchText={setSearchText}
+                        tooltip={t('filterEventBy', { filter: `BlockNo=${item.blockNumber}` })}
+                        value={`${item.blockNumber}`}
+                      />
+                    </Box>
+                  </Stack>
+                </TableCell>
+                {/* <TableCell>{item.method}</TableCell> */}
+                <TableCell sx={{ pr: 1 }}>
+                  <ContractEventListItem item={item} setSearchText={setSearchText} />
                 </TableCell>
               </TableRow>
-            )}
+            ))}
           </TableBody>
         </Table>
       </TableContainer>
+    </Box>
+  ) : (
+    <Box>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: '8.75rem 0',
+          color: 'var(--primary-color)',
+        }}
+      >
+        <NoDataIcon />
+        <span style={{ marginTop: '2rem', color: 'var(--primary-text-color)' }}>{t(`no_records`)}</span>
+      </div>
     </Box>
   )
 }
