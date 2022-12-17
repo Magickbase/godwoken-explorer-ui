2dfe2debf84eba98a50f925abf17b161ae234ffe
qiweiii <yangqiwei97@gmail.com>
2022-12-17 13:48:34 +0800

    fix: tooltip

--- components/Tooltip.tsx
+++ components/Tooltip.tsx
@@ -2,7 +2,14 @@ import Tooltip, { TooltipProps, tooltipClasses } from '@mui/material/Tooltip'
 import { styled } from '@mui/material/styles'
 
 export default styled(({ className, ...props }: TooltipProps) => (
-  <Tooltip TransitionProps={{ timeout: 200 }} arrow classes={{ popper: className, tooltip: className }} {...props} />
+  <Tooltip
+    TransitionProps={{ timeout: 200 }}
+    arrow
+    classes={{ popper: className, tooltip: className }}
+    enterTouchDelay={0}
+    leaveTouchDelay={3000}
+    {...props}
+  />
 ))(({ theme }) => ({
   [`& .${tooltipClasses.arrow}`]: {
     color: theme.palette.common.black,
