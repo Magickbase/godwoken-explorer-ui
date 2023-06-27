b6e4984af0bbe964f57cda2b2c765cb8d3c7021c
Jun Ma <ma82180597@163.com>
2023-06-27 11:40:15 +0800

    fix: SubDID is resolved to its parent DataDID page

--- components/AccountOverview/index.tsx
+++ components/AccountOverview/index.tsx
@@ -351,7 +351,13 @@ const AccountOverview: React.FC<AccountOverviewProps & { refetch: () => Promise<
 
   const getInfoBlock = account => {
     const { type } = account
-    const domain = account?.bit_alias
+    let domain = account?.bit_alias
+
+    if (domain && domain.includes('#')) {
+      const [domainName, subDomain] = domain.split('#')
+
+      domain = `${subDomain.split('.')[0]}.${domainName}.bit`
+    }
 
     const blockMap = {
       [`${GraphQLSchema.AccountType.MetaContract}`]: <MetaContract {...(account.script as MetaContract['script'])} />,
